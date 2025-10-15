f3a521dd67da1cdf33793ba2829f4f2c2624545a
Gustavo Fernandes <fgustavo.santoss@gmail.com>
2025-10-15 20:40:15 -0300

    tutorial com threads

--- src/main.c
+++ src/main.c
@@ -2,11 +2,10 @@
 #include <zephyr/device.h>
 #include <zephyr/drivers/gpio.h>
 
-#define SLEEP_TIME_MS 250
+#define SLEEP_TIME_MS 500
 
 // Define o LED usando Device Tree
 #define LED0_NODE DT_ALIAS(led0)
-#define LED1_NODE DT_ALIAS(led1)
 
 // Verifica se o LED está definido no Device Tree
 #if DT_NODE_HAS_STATUS(LED0_NODE, okay)
@@ -14,24 +13,26 @@ static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
 #else
 #error "Unsupported board: led0 devicetree alias is not defined"
 #endif
-#if DT_NODE_HAS_STATUS(LED1_NODE, okay)
-static const struct gpio_dt_spec led1 = GPIO_DT_SPEC_GET(LED1_NODE, gpios);
-#else
-#error "Unsupported board: led0 devicetree alias is not defined"
-#endif
+
+#define STACK_SIZE 256
+#define PRIORITY 7
+
+void funcao_toggle(void){
+    while (1) {
+        gpio_pin_toggle_dt(&led);
+        k_msleep(SLEEP_TIME_MS);
+    }
+}
+
+K_THREAD_DEFINE(toggle, STACK_SIZE, funcao_toggle, NULL, NULL, NULL, PRIORITY, 0, 0);
 
 void main(void)
 {
-    int ret, ret1;
-    int timer = 0;
+    int ret;
 
     // Verifica se o device está pronto
     if (!gpio_is_ready_dt(&led)) {
         printk("Error: LED device %s is not ready\n", led.port->name);
-        return;
-    }
-    if (!gpio_is_ready_dt(&led1)) {
-        printk("Error: LED device %s is not ready\n", led1.port->name);
         return;
     }
 
@@ -41,26 +42,6 @@ void main(void)
         printk("Error %d: failed to configure LED pin\n", ret);
         return;
     }
-    ret1 = gpio_pin_configure_dt(&led1, GPIO_OUTPUT_ACTIVE);
-    if (ret1 < 0) {
-        printk("Error %d: failed to configure LED pin\n", ret1);
-        return;
-    }
 
     printk("LED blinking on %s pin %d\n", led.port->name, led.pin);
-    printk("LED blinking on %s pin %d\n", led1.port->name, led1.pin);
-
-    while (1) {
-        // Toggle do LED usando a nova API
-        if(timer%7 == 0){
-            gpio_pin_toggle_dt(&led);
-        }
-
-        if(timer%20 == 0){
-            gpio_pin_toggle_dt(&led1);
-        }
-
-        timer++;
-        k_msleep(25);
-    } 
 }
