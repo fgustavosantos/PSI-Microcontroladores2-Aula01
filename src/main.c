0b031d411ff92165b7ec47a83c0679fa6726d0cc
Gustavo Fernandes <fgustavo.santoss@gmail.com>
2025-10-14 23:11:38 -0300

    led0 a 1s e led1 a 500ms

--- src/main.c
+++ src/main.c
@@ -2,10 +2,11 @@
 #include <zephyr/device.h>
 #include <zephyr/drivers/gpio.h>
 
-#define SLEEP_TIME_MS 500
+#define SLEEP_TIME_MS 250
 
 // Define o LED usando Device Tree
 #define LED0_NODE DT_ALIAS(led0)
+#define LED1_NODE DT_ALIAS(led1)
 
 // Verifica se o LED está definido no Device Tree
 #if DT_NODE_HAS_STATUS(LED0_NODE, okay)
@@ -13,14 +14,23 @@ static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
 #else
 #error "Unsupported board: led0 devicetree alias is not defined"
 #endif
+#if DT_NODE_HAS_STATUS(LED1_NODE, okay)
+static const struct gpio_dt_spec led1 = GPIO_DT_SPEC_GET(LED1_NODE, gpios);
+#else
+#error "Unsupported board: led0 devicetree alias is not defined"
+#endif
 
 void main(void)
 {
-    int ret;
+    int ret, ret1;
 
     // Verifica se o device está pronto
     if (!gpio_is_ready_dt(&led)) {
         printk("Error: LED device %s is not ready\n", led.port->name);
+        return;
+    }
+    if (!gpio_is_ready_dt(&led1)) {
+        printk("Error: LED device %s is not ready\n", led1.port->name);
         return;
     }
 
@@ -30,12 +40,23 @@ void main(void)
         printk("Error %d: failed to configure LED pin\n", ret);
         return;
     }
+    ret1 = gpio_pin_configure_dt(&led1, GPIO_OUTPUT_ACTIVE);
+    if (ret1 < 0) {
+        printk("Error %d: failed to configure LED pin\n", ret1);
+        return;
+    }
 
     printk("LED blinking on %s pin %d\n", led.port->name, led.pin);
+    printk("LED blinking on %s pin %d\n", led1.port->name, led1.pin);
 
     while (1) {
-        // Toggle do LED usando a nova API
-        gpio_pin_toggle_dt(&led);
+        // Toggle do LED usando a nova API 
         k_msleep(SLEEP_TIME_MS);
-    }
+        gpio_pin_toggle_dt(&led1);
+
+        k_msleep(SLEEP_TIME_MS);
+        gpio_pin_toggle_dt(&led1);
+        gpio_pin_toggle_dt(&led);        
+
+    } 
 }
