ca2c26b77674c5e3c5349562b714d6d2a3bf366e
Gustavo Fernandes <fgustavo.santoss@gmail.com>
2025-10-15 21:19:46 -0300

    led0 a 1s e led1 a 500ms com threads

--- src/main.c
+++ src/main.c
@@ -2,10 +2,12 @@
 #include <zephyr/device.h>
 #include <zephyr/drivers/gpio.h>
 
-#define SLEEP_TIME_MS 500
+#define SLEEP_TIME_LED0 500
+#define SLEEP_TIME_LED1 250
 
 // Define o LED usando Device Tree
 #define LED0_NODE DT_ALIAS(led0)
+#define LED1_NODE DT_ALIAS(led1)
 
 // Verifica se o LED está definido no Device Tree
 #if DT_NODE_HAS_STATUS(LED0_NODE, okay)
@@ -14,34 +16,58 @@ static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
 #error "Unsupported board: led0 devicetree alias is not defined"
 #endif
 
+#if DT_NODE_HAS_STATUS(LED1_NODE, okay)
+static const struct gpio_dt_spec led1 = GPIO_DT_SPEC_GET(LED1_NODE, gpios);
+#else
+#error "Unsupported board: led0 devicetree alias is not defined"
+#endif
+
 #define STACK_SIZE 256
 #define PRIORITY 7
 
-void funcao_toggle(void){
+void piscaLed0(void){
     while (1) {
         gpio_pin_toggle_dt(&led);
-        k_msleep(SLEEP_TIME_MS);
+        k_msleep(SLEEP_TIME_LED0);
     }
 }
 
-K_THREAD_DEFINE(toggle, STACK_SIZE, funcao_toggle, NULL, NULL, NULL, PRIORITY, 0, 0);
+void piscaLed1(void){
+    while (1) {
+        gpio_pin_toggle_dt(&led1);
+        k_msleep(SLEEP_TIME_LED1);
+    }
+}
+
+K_THREAD_DEFINE(toggle_led0, STACK_SIZE, piscaLed0, NULL, NULL, NULL, PRIORITY, 0, 0);
+K_THREAD_DEFINE(toggle_led1, STACK_SIZE, piscaLed1, NULL, NULL, NULL, PRIORITY, 0, 0);
 
 void main(void)
 {
-    int ret;
+    int ret, ret1;
 
     // Verifica se o device está pronto
     if (!gpio_is_ready_dt(&led)) {
-        printk("Error: LED device %s is not ready\n", led.port->name);
+        printk("Error: LED0 device %s is not ready\n", led.port->name);
+        return;
+    }
+    if (!gpio_is_ready_dt(&led1)) {
+        printk("Error: LED1 device %s is not ready\n", led1.port->name);
         return;
     }
 
     // Configura o pino como saída
     ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
     if (ret < 0) {
-        printk("Error %d: failed to configure LED pin\n", ret);
+        printk("Error %d: failed to configure LED0 pin\n", ret);
+        return;
+    }
+    ret1 = gpio_pin_configure_dt(&led1, GPIO_OUTPUT_ACTIVE);
+    if (ret1 < 0) {
+        printk("Error %d: failed to configure LED1 pin\n", ret1);
         return;
     }
 
-    printk("LED blinking on %s pin %d\n", led.port->name, led.pin);
+    printk("LED0 blinking on %s pin %d\n", led.port->name, led.pin);
+    printk("LED1 blinking on %s pin %d\n", led1.port->name, led1.pin);
 }
